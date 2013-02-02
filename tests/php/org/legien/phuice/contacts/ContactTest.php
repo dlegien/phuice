549091d24f5f1e340f1ffeda0efb431596f5d694
Daniel Legien <daniel@legien.org>
2013-02-03 00:40:23 +0100

    Added tests folder

---
+++ tests/php/org/legien/phuice/contacts/ContactTest.php
@@ -0,0 +1,16 @@
+<?php
+
+	use org\legien\phuice\testing\TestBase;
+	use org\legien\phuice\contacts\Contact;
+	use org\legien\phuice\contacts\Name;
+
+	class ContactTest extends TestBase {
+
+		public function testConstruction() {
+
+			$c = new Contact(new Name('Daniel', 'Legien'), 'daniel@legien.org');
+			$this->assertEquals($c->getName()->getFirstname(), 'Daniel');
+
+		}
+
+	}
