298a5e1ccc0656e1c627c85f3b18ae25e2f1c03a
Daniel Legien <daniel@legien.org>
2013-02-03 23:02:22 +0100

    Testing

--- src/test/php/org/legien/phuice/routing/DefaultRouterTest.php
+++ src/test/php/org/legien/phuice/routing/DefaultRouterTest.php
@@ -3,6 +3,7 @@
 	use org\legien\phuice\routing\DefaultRouter;
 	use org\legien\phuice\routing\MockCallTarget;
 	use org\legien\phuice\routing\Route;
+	use org\legien\phuice\routing\RouteList;
 	use org\legien\phuice\services\ServiceDirectory;
 	use org\legien\phuice\testing\TestBase;
 
@@ -25,6 +26,36 @@
 			$router->route('mock/call/2');
 
 			$this->assertTrue($this->serviceDirectory->getService('mock')->gotCalled());
+		}
+		
+		public function testAddingListOfRoutes() {
+			
+			$this->logger->debug('Testing if a list of routes can be added to the router.');
+			
+			$routeList = new RouteList();
+			$routeList->add(new Route('abc'));
+			$routeList->add(new Route('def'));
+
+			$routeList2 = new RouteList();
+			$routeList2->add(new Route('ghi'));
+
+			$router = new DefaultRouter($this->serviceDirectory, $routeList);
+
+			$routecount = 0;
+			foreach($router->getRoutes() as $route) {
+				$routecount++;
+			}
+
+			$this->assertEquals(2, $routecount);
+
+			$router->setRoutes($routeList2);
+
+			$routecount2 = 0;
+			foreach($router->getRoutes() as $route) {
+				$routecount2++;
+			}
+
+			$this->assertEquals(1, $routecount2);
 		}
 	
 	}
